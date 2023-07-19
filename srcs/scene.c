5e224a08b844668a03071081b5b206fa11a06d02
Haoov <rpasabb@gmail.com>
2023-07-19 11:44:12 +0200

    norm start

--- srcs/scene.c
+++ srcs/scene.c
@@ -9,116 +9,59 @@ int	scene_render(t_app *app, t_scene *scene, t_img *img)
 //******************************************************************************
 //Create some material
 //******************************************************************************
-	t_material	earth;
-	t_material	atm;
-	t_material	moon;
-	t_material	reflec;
+	t_material	mat;
 
-	earth = simple_mat_const(vec_create(1.0, 1.0, 1.0), 0.0, 0.0);
-	atm = refractive_mat_const(vec_create(0.7, 0.9, 1.0), 0.0, 0.0, 0.6);
-	moon = simple_mat_const(vec_create(1.0, 1.0, 1.0), 0.02, 0.5);
-	reflec = simple_mat_const(vec_create(0.2, 0.4, 0.65), 0.4, 10.0);
+	mat = simple_mat_const(vec_create(0.0, 0.0, 1.0), 0.5, 0.0);
 
 //******************************************************************************
 //Create some textures
 //******************************************************************************
-	t_texture	earth_img;
-	t_texture	clouds_img;
-	t_texture	moon_img;
 
-	earth_img = img_txt_create();
-	load_image(app, &earth_img, "./imgs/earth8k.xpm");
-	earth_img.tfm = set_transform(vec_create(0.0, 0.0, 0.0), 0.0,
-								  vec_create(1.0, 1.0, 1.0));
-	assign_texture(&earth, earth_img);
-
-	clouds_img = img_txt_create();
-	load_image(app, &clouds_img, "./imgs/earth_clouds.xpm");
-	clouds_img.tfm = set_transform(vec_create(0.0, 0.0, 0.0), 0.0,
-								   vec_create(1.0, 1.0, 1.0));
-	assign_texture(&atm, clouds_img);
-
-	moon_img = img_txt_create();
-	load_image(app, &moon_img, "./imgs/moon8k.xpm");
-	moon_img.tfm = set_transform(vec_create(0.0, 0.0, 0.0), 0.0,
-								 vec_create(1.0, 1.0, 1.0));
-	assign_texture(&moon, moon_img);
 
 //******************************************************************************
 //Create normal map
 //******************************************************************************
-	t_normal_map	earth_map;
-	t_normal_map	water_map;
 
-	earth_map = normal_map_create();
-	nmap_load_image(app, &earth_map, "./normal_maps/earth8k_norm.xpm");
-	earth_map.tfm = set_transform(vec_create(0.0, 0.0, 0.0), M_PI_2,
-								  vec_create(1.0, 1.0, 1.0));
-	assign_normal_map(&earth, earth_map);
-
-	water_map = normal_map_create();
-	nmap_load_image(app, &water_map, "./normal_maps/Water-0341normal.xpm");
-	water_map.tfm = set_transform(vec_create(0.0, 0.0, 0.0), 0.0,
-								  vec_create(1.5, 1.5, 1.0));
-	assign_normal_map(&reflec, water_map);
 
 //******************************************************************************
 //Configure the camera
 //******************************************************************************
     cam_init(&scene->cam);
-	scene->cam.pos = vec_create(0.0, -10.0, 0.0);
+	scene->cam.pos = vec_create(0.0, -5.0, -2.0);
 	scene->cam.look_at = vec_create(0.0, 0.0, 0.0);
 	scene->cam.up = vec_create(0.0, 0.0, 1.0);
-	scene->cam.size = 0.25;
+	scene->cam.size = 0.75;
 	scene->cam.ratio = 16.0 / 9.0;
     cam_geometry(&scene->cam);
 
 //******************************************************************************
 //Create some objects
 //******************************************************************************
-	t_obj_lst	*earth_obj;
-	t_obj_lst	*atm_obj;
-	t_obj_lst	*moon_obj;
 	t_obj_lst	*plan;
-
-	earth_obj = sphere_create();
-	earth_obj->gtfm = gtfm_set(vec_create(0.0, 0.0, 0.0),
-							   vec_create(-30.0, 0.0, -80.0),
-							   vec_create(0.5, 0.5, 0.5));
-	assign_material(earth_obj, earth);
-
-	atm_obj = sphere_create();
-	atm_obj->gtfm = gtfm_set(vec_create(0.0, 0.0, 0.0),
-							   vec_create(-30.0, 0.0, -80.0),
-							   vec_create(0.51, 0.51, 0.51));
-	assign_material(atm_obj, atm);
-
-	moon_obj = sphere_create();
-	moon_obj->gtfm = gtfm_set(vec_create(5.0, 28.0, -2.0),
-							  vec_create(0.0, 0.0, 0.0),
-							  vec_create(0.15, 0.15, 0.15));
-	assign_material(moon_obj, moon);
+	t_obj_lst	*sphere;
 
 	plan = plane_create();
-	plan->gtfm = gtfm_set(vec_create(0.0, 0.0, 0.5),
-						  vec_create(-20.0, 0.0, 0.0),
-						  vec_create(1.5, 1.5, 1.5));
-	assign_material(plan, reflec);
+	plan->gtfm = gtfm_set(vec_create(0.0, 0.0, 1.0),
+						  vec_create(0.0, 0.0, 0.0),
+						  vec_create(4.0, 4.0, 1.0));
+	sphere = sphere_create();
+	sphere->gtfm = gtfm_set(vec_create(0.0, 0.0, 0.0),
+							vec_create(0.0, 0.0, 0.0),
+							vec_create(1.0, 1.0, 1.0));
+	assign_material(sphere, mat);
 
 //******************************************************************************
 //Add objects to the scene
 //******************************************************************************
-	add_obj(&scene->obj_lst, earth_obj);
-	add_obj(&scene->obj_lst, atm_obj);
-	add_obj(&scene->obj_lst, moon_obj);
 	add_obj(&scene->obj_lst, plan);
+	add_obj(&scene->obj_lst, sphere);
 
 //******************************************************************************
 //Create lights
 //******************************************************************************
 	if (add_light(&scene->light_lst, POINT) == NULL)
 		return (FAILURE);
-	scene->light_lst->light.pos = vec_create(0.0, -10.0, 0.0);
+	scene->light_lst->light.pos = vec_create(5.0, -10.0, -5.0);
 	scene->light_lst->light.color = vec_create(1.0, 1.0, 1.0);
 
     //Loop over every pixel
